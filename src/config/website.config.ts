6b6ce330b6b66a7ad4b56902e0ad06fd976fb819
pengshixian <61853492+sx565355044@users.noreply.github.com>
2023-05-16 17:17:02 +0800

    Update website.config.ts

--- src/config/website.config.ts
+++ src/config/website.config.ts
@@ -2,7 +2,7 @@ import logoImage from '@/assets/logo.png'
 import loginImage from '@/assets/login-logo.png'
 
 export const websiteConfig = Object.freeze({
-  title: '三生宇宙',
+  title: '一贤科技',
   logo: logoImage,
   loginImage,
   loginDesc: '',
